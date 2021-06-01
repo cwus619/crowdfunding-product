b1d8d358cdab311995a0f53a8f2b5395c97ff896
cwus619 <71528072+cwus619@users.noreply.github.com>
2021-06-01 22:55:46 +0100

    Update index.js
    Function to respond to radio form selection implemented, change to outOfStock to reflect removal of divs

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ function main(){
     const bookmark = document.querySelector(".bookmark");
     const products= document.querySelectorAll(".product");
     const modalProducts = document.querySelectorAll(".product-modal");
-    // const backProjectBtns = document.querySelectorAll(".reward-btn");
     const backProjectBtns = document.querySelectorAll(".reward-btn");
+    const closeBox = document.querySelector(".close");
+    const pledgeEntryBoxes = document.querySelectorAll(".pledge-entry")
+    const formCheckInputs = document.querySelectorAll(".form-check-input")
+
+
     outOfStock(products, modalProducts);
     bookmarkPage(bookmark);    
-    modalBoxControl(backProjectBtns)
+    modalBoxControl(backProjectBtns, closeBox);
+    radioForm(formCheckInputs, pledgeEntryBoxes);
 }
 
 
@@ -30,9 +35,7 @@ function outOfStock(products, modalProducts){
     for (let mProduct of modalProducts){
         const mRemainder = mProduct.children[0].children[3].children[0];
         if (mRemainder.innerHTML === "0"){
-            console.log(`Remainder is: ${mRemainder.innerHTML}`)
             const radioBtn = mProduct.children[0].children[0];
-            console.log(mProduct.children[0].children[2])
             mProduct.style.opacity = "0.4";
             radioBtn.disabled = "true";
         }
@@ -42,31 +45,52 @@ function outOfStock(products, modalProducts){
 // change color of bookmark button to indicate bookmarked
 function bookmarkPage(bookmark){
     bookmark.addEventListener("click", function(){
-        console.log("Bazinga");
         const bookmarkIcon = document.querySelector(".bookmark-icon") 
         const circle =  bookmarkIcon.children[0].children[0];
         const path = bookmarkIcon.children[0].children[1];
         if (bookmark.style.color === ""){
             bookmark.style.color = "hsl(176, 50%, 47%)";
+            
             circle.style.fill = "hsl(176, 50%, 47%)";
             path.style.fill = "#FFFFFF";
         } else{
             bookmark.style.color = "";
+            
             circle.style.fill = "#2F2F2F";
             path.style.fill = "##B1B1B1"
         }
     })
 }
 
-const backProjectBtns = document.querySelectorAll(".reward-btn");
-
 // modal box control
-
-function modalBoxControl(backProjectBtns){
+function modalBoxControl(backProjectBtns, close){
     for (let backBtn of backProjectBtns){
         backBtn.addEventListener("click", function(){
             document.querySelector(".modal-bg").style.display = "flex";
         })
+    }
+    close.addEventListener("click", function(){
+        document.querySelector(".modal-bg").style.display = "none";
+    })
+}
+
+// radio form check
+function radioForm(formCheckInputs, pledgeEntryBoxes){
+    for (let check of formCheckInputs){
+        const pledgeEntry = check.parentNode.parentNode.children[2];
+        $(document).ready(function(){
+            $(check).click(function(){
+                console.log(pledgeEntry)
+                if (check.checked){
+                    // loop over all pledgeEntryBoxes to close them
+                    for (let pledgeBox of pledgeEntryBoxes){
+                        pledgeBox.style.display = "none";
+                    }
+                    // display only selected pledgeEntry
+                    pledgeEntry.style.display = "flex";
+                }
+            })
+        })          
     }
 }
 
